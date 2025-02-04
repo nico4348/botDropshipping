3c3c12c5b1a1c552ac4494149488e32e53d10af7
nico4348 <nico4348@gmail.com>
2025-02-04 11:41:31 -0500

    botDropshipping

---
+++ src/app.js
@@ -0,0 +1,67 @@
+import { join } from 'path'
+import { createBot, createProvider, createFlow, addKeyword, utils } from '@builderbot/bot'
+import { MysqlAdapter as Database } from '@builderbot/database-mysql'
+import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
+
+const PORT = process.env.PORT ?? 3008
+
+const main = async () => {
+	const adapterFlow = createFlow([welcomeFlow])
+
+	const adapterProvider = createProvider(Provider)
+	const adapterDB = new Database({
+		host: process.env.MYSQL_DB_HOST,
+		user: process.env.MYSQL_DB_USER,
+		database: process.env.MYSQL_DB_NAME,
+		password: process.env.MYSQL_DB_PASSWORD,
+	})
+
+	const { handleCtx, httpServer } = await createBot({
+		flow: adapterFlow,
+		provider: adapterProvider,
+		database: adapterDB,
+	})
+
+	adapterProvider.server.post(
+		'/v1/messages',
+		handleCtx(async (bot, req, res) => {
+			const { number, message, urlMedia } = req.body
+			await bot.sendMessage(number, message, { media: urlMedia ?? null })
+			return res.end('sended')
+		})
+	)
+
+	adapterProvider.server.post(
+		'/v1/register',
+		handleCtx(async (bot, req, res) => {
+			const { number, name } = req.body
+			await bot.dispatch('REGISTER_FLOW', { from: number, name })
+			return res.end('trigger')
+		})
+	)
+
+	adapterProvider.server.post(
+		'/v1/samples',
+		handleCtx(async (bot, req, res) => {
+			const { number, name } = req.body
+			await bot.dispatch('SAMPLES', { from: number, name })
+			return res.end('trigger')
+		})
+	)
+
+	adapterProvider.server.post(
+		'/v1/blacklist',
+		handleCtx(async (bot, req, res) => {
+			const { number, intent } = req.body
+			if (intent === 'remove') bot.blacklist.remove(number)
+			if (intent === 'add') bot.blacklist.add(number)
+
+			res.writeHead(200, { 'Content-Type': 'application/json' })
+			return res.end(JSON.stringify({ status: 'ok', number, intent }))
+		})
+	)
+
+	httpServer(+PORT)
+}
+
+main()
